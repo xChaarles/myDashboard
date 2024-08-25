c7eda7924bc99ddbfcdf636334a70e5a9cbcbd45
xchaarles <carlosbp2608@gmail.com>
2024-08-25 18:04:19 -0500

    Configuraciond e navegacion de rutas visual

---
+++ src/app/shared/sidemenu/sidemenu.component.ts
@@ -0,0 +1,21 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { routes } from '../../app.routes';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-sidemenu',
+  standalone: true,
+  imports: [CommonModule, RouterModule],
+  templateUrl: './sidemenu.component.html',
+  styles: '',
+})
+export class SidemenuComponent {
+
+  public menuItems = routes
+    .map(route => route.children ?? [])
+    .flat()
+    .filter (route => route && route.path)
+    .filter (route => !route.path?.includes(':')) 
+
+ }
