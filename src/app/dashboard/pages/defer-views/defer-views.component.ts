327c81a04a1d4c872b4e8a724aa23a3d1a5097e3
xchaarles <carlosbp2608@gmail.com>
2024-08-26 15:59:17 -0500

    configuracion de defers block

--- src/app/dashboard/pages/defer-views/defer-views.component.ts
+++ src/app/dashboard/pages/defer-views/defer-views.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { HeavyLoadersSlowComponent } from '../../../shared/heavy-loaders/heavy-loaders-slow.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   standalone: true,
-  imports: [],
+  imports: [HeavyLoadersSlowComponent, CommonModule],
   templateUrl: './defer-views.component.html',
   styles: ``
 })
--- src/app/shared/heavy-loaders/heavy-loaders-slow.component.ts
+++ src/app/shared/heavy-loaders/heavy-loaders-slow.component.ts
@@ -1,12 +1,20 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-heavy-loaders-slow',
   standalone: true,
-  imports: [
-    CommonModule,
-  ],
-  template: `<p>heavy-loaders-slow works!</p>`,
+  imports: [CommonModule],
+  template: `<section [ngClass]="['w-full h-[300px]', cssClass]">
+    Heavy Loaders slow
+  </section>`,
 })
-export class HeavyLoadersSlowComponent { }
+export class HeavyLoadersSlowComponent { 
+
+  @Input({required: true}) cssClass!: string;
+
+  constructor(){
+      const star = Date.now();
+      while (Date.now() - star < 3000 );
+  }
+}
